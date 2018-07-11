f599eade3a439ec73ae6dd642fd314ab7467c649
Tosho Ivanov <todor.ait@gmail.com>
2018-07-11 23:59:16 +0300

    updated labels

--- server/res/msg.js
+++ server/res/msg.js
@@ -1,15 +1,31 @@
 module.exports = {
 
-    USER_REGISTER_CANT_CREATE: 'Could not create user.',
-    USER_REGISTER_PWDS_NOMATCH: 'Passwords do not match.',
-    USER_REGISTER_EMAIL_TAKEN: 'A user with this email already exists.',
+    LOGIN_FAILED: 'Incorrect email or password.',
 
-    USER_REGISTER_REGISTER_DONE: 'New account registered.',
+    USER_REGISTER_FAILED: "Could not create user.",
+    USER_REGISTER_DONE: "New account registered.",
 
-    USER_LOGIN_FAILED: 'Incorrect User or Password.',
+    USER_RESET_DONE: '--',
+    USER_RESET_FAILED: "Could not reset password.",
+
+    USER_UPDATE_FAILED: "Could not update account.",
 
 
+    EMAIL_REQUIRED: "Enter an email.",
+    EMAIL_TAKEN: "Email already taken.",
 
+    USERNAME_REQUIRED: "Enter a name.",
+    USERNAME_MIN8: "Name must be at least 8 characters.",
+    USERNAME_MAX25: "Name must be no more than 25 characters.",
+
+    PASSWORD_REQUIRED: "Enter a password.",
+    PASSWORD_MIN8: "Password must be at least 8 characters.",
+    PASSWORD_MAX25: "Password must be no more than 25 characters.",
+
+    PASSCONFIRM_REQUIRED: "Enter password again.",
+    PASS_NOMATCH: "Password is not correct.",
+
+    PASSCONFIRM_NOMATCH: "Passwords do not match.",
 
 
 }
