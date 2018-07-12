969f75aca0fc558bf45db02ab6b9b26d1b866988
Tosho Ivanov <todor.ait@gmail.com>
2018-07-12 23:18:16 +0300

    upd labels

--- client/config/labels.js
+++ client/config/labels.js
@@ -1,33 +1,14 @@
-export const LBL_BTN_CLOSE = "Fermer"
-export const LBL_BTN_CONFIRM = "Confirmer"
-
-export const LBL_LBL_ATTENTION = "Attention"
+export const LBL_BTN_CLOSE = "Close"
 
 export const LBL_MSG_INFO = "Info"
 export const LBL_MSG_WARNING = "Attention"
-export const LBL_MSG_ERROR = "Erreur"
-export const LBL_MSG_SUCCESS = "Enregistrement effectué"
+export const LBL_MSG_ERROR = "Error"
+export const LBL_MSG_SUCCESS = "Done"
 
-export const LBL_FT_NEXT = "suivante"
-export const LBL_FT_PREV = "précédente"
+export const LBL_FT_NEXT = "next"
+export const LBL_FT_PREV = "prev"
 
-export const LBL_FL_SELECT = 'sélectionner...'
 
-export const LBL_FDMS_SELECT = "Sélectionner..."
-export const LBL_FDMS_MONDAY_SHORT = "Lu"
-export const LBL_FDMS_TUESDAY_SHORT = "Ma"
-export const LBL_FDMS_WEDNESDAY_SHORT = "Me"
-export const LBL_FDMS_THURSDAY_SHORT = "Je"
-export const LBL_FDMS_FRIDAY_SHORT = "Ve"
-export const LBL_FDMS_SATURDAY_SHORT = "Sa"
-export const LBL_FDMS_SUNDAY_SHORT = "Di"
-
-export const LBL_FMS_SELECT = 'sélectionner...'
-export const LBL_FMS_SELECT_ALL = 'Sélectionner tout'
-export const LBL_FMS_SELECTED_MULTIPLE = 'sélectionnées'
-
-export const LBL_RC_CONFIRM_L1 = "Etes-vous sûr de vouloir quitter la page?"
-export const LBL_RC_CONFIRM_L2 = "Vous avez des données qui ne sont pas enregistrées."
 
 
 
